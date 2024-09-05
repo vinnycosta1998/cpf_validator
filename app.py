24913841500740fccb8de81e1f1e0fb567757ca2
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-09-05 08:01:24 -0700

    create method screen

--- app.py
+++ app.py
@@ -2,4 +2,16 @@ from tkinter import *
 
 root = Tk()
 
-root.mainloop()
+class Application():
+    def __init__(self):
+        self.root = root
+        self.screen()
+        root.mainloop()
+
+    def screen(self):
+        self.root.configure(background="#000")
+        self.root.geometry("240x240")
+        self.root.resizable(False, False)
+        self.root.title("CPF Validator")
+
+Application()
