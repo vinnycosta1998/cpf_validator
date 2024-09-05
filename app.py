9ad1f738a70c30825c065e66bf749f80d00481d7
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-09-05 10:01:19 -0700

    create select_file method for select excel files

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
 
 root = Tk()
 
@@ -7,6 +9,7 @@ class Application():
         self.root = root
         self.screen()
         self.frames()
+        self.buttons()
         root.mainloop()
 
     def screen(self):
@@ -16,7 +19,23 @@ class Application():
         self.root.title("CPF Validator")
 
     def frames(self):
-        self.container = Frame(self.root, bg="#fff")
+        self.container = Frame(self.root)
         self.container.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.96) 
+
+    def select_file(self):
+        self.file_path = filedialog.askopenfilename(
+            title="Selecione um arquivo Excel",
+            filetypes=["Arquivos Excel", "*.xls *.xlsx"]
+        )
+
+        if self.file_path:
+            messagebox.showinfo("Arquivo Selecionado", f"Arquivo selecionado, ${self.file_path}")
+        else:
+            messagebox.showinfo("Nenhum arquivo sleecionado", "Nenhum arquivo foi selecionado")
+    
+    def buttons(self):
+        self.button_open_file = Button(self.container, text="Selecione um arquivo Excel", command=self.select_file)
+        self.button_open_file.place(relx=0.05, rely=0.4, relwidth=0.90, relheight=0.15)
+
 
 Application()
