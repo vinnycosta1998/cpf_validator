7ac741c4053006b461c249773e03b83d70d4dac6
Vinicius <viniciuscostadealmeida98@gmail.com>
2024-09-05 08:06:18 -0700

    create container on gui

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ class Application():
     def __init__(self):
         self.root = root
         self.screen()
+        self.frames()
         root.mainloop()
 
     def screen(self):
@@ -13,5 +14,9 @@ class Application():
         self.root.geometry("240x240")
         self.root.resizable(False, False)
         self.root.title("CPF Validator")
+
+    def frames(self):
+        self.container = Frame(self.root, bg="#fff")
+        self.container.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.96) 
 
 Application()
